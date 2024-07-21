4432829e9b61d49ea858f6e56e6ad950f4ac6a1c
Laylfrn <146813327+Laylfrn@users.noreply.github.com>
2024-07-21 15:39:23 +0000

    New Update

--- Group-7/view/product.php
+++ Group-7/view/product.php
@@ -9,7 +9,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Seven Eleven</title>
-  <link rel="stylesheet" href="assets/css/home.css">
+  <link rel="stylesheet" href="assets/css/product.css">
   <link rel="stylesheet" href="assets/css/home-reponsive.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -35,7 +35,30 @@
   
 <main>
 
+  <div class="sample-container">
+    <h1>What's new</h1>
+  </div>
 
+  <div class="food-container">
+  <img src="assets/img/product-img/cake-roll.jpg" alt="cake-roll">
+  <img src="assets/img/product-img/cheese-hot.jpg" alt="cheese-hot">
+  <img src="assets/img/product-img/chicken-burger.jpg" alt="chicken-burger">
+  
+  </div>
+
+  <div class="food-container">
+  <img src="assets/img/product-img/chicken-fillet.jpg" alt="cake-roll">
+  <img src="assets/img/product-img/cookies.jpg" alt="cheese-hot">
+  <img src="assets/img/product-img/frank.jpg" alt="chicke-burger">
+  
+  </div>
+
+  <div class="food-container">
+  <img src="assets/img/product-img/hotdog.jpg" alt="cake-roll">
+  <img src="assets/img/product-img/meat-balls.jpg" alt="cheese-hot">
+  <img src="assets/img/product-img/siopao.jpg" alt="chicke-burger">
+  
+  </div>
 
 </main>
     
